7dbcb13e4e51bdc4682e502d7dab2c51df8190de
Erwin Staal <erwin.staal@klm.com>
2024-10-30 10:13:32 +0100

    Add required_providers and provider block for azurerm

--- Environments/Demo-Terraform/main.tf
+++ Environments/Demo-Terraform/main.tf
@@ -1,4 +1,17 @@
 
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "3.105.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
+
 resource "azurerm_storage_account" "demo-azurerm_storage_account" {
   name                     = "stg${var.ade_env_name}${var.env_name}${var.ade_environment_type}"
   resource_group_name      = var.resource_group_name

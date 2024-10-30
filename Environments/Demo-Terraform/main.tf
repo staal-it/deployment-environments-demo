28f57211c92db48b14bb21aaad14040dc68da762
Erwin Staal <erwin.staal@klm.com>
2024-10-30 10:31:25 +0100

    Update storage account name to lowercase

--- Environments/Demo-Terraform/main.tf
+++ Environments/Demo-Terraform/main.tf
@@ -13,7 +13,7 @@ provider "azurerm" {
 }
 
 resource "azurerm_storage_account" "demo-azurerm_storage_account" {
-  name                     = "stg${var.ade_env_name}${var.env_name}${var.ade_environment_type}"
+  name                     = lower("stg${var.ade_env_name}${var.env_name}${var.ade_environment_type}")
   resource_group_name      = var.resource_group_name
   location                 = var.ade_location
   account_tier             = "Standard"
